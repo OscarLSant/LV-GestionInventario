f10cca8414bda0570d1bdab41770d02269a0a9ba
oscarLS-11 <oluna1103@gmail.com>
2023-07-04 12:16:46 -0600

    busqueda clientes no funciona

--- database/seeders/seederCategorias.php
+++ database/seeders/seederCategorias.php
@@ -12,6 +12,6 @@ class seederCategorias extends Seeder
      */
     public function run(): void
     {
-        \App\Models\modelCategorias::factory(5)->create();
+        \App\Models\modelCategorias::factory(15)->create();
     }
 }
